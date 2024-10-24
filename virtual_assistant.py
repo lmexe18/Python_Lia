d82fcd8b0b8945e155f344f5d58e59ce25f7e970
Laura Pedraza <laurampego@gmail.com>
2024-10-24 17:52:59 +0200

    Estructura inicial

---
+++ virtual_assistant.py
@@ -0,0 +1,98 @@
+import datetime
+import pyttsx3
+import speech_recognition as sr
+import yfinance as yf
+import webbrowser
+
+def audio_to_text():
+    
+    # Recognizer
+    r = sr.Recognizer()
+    
+    # Configuramos el micro
+    with sr.Microphone() as origin:
+        # Tiempo de espera desde que se activa el micro
+        r.pause_threshold = 0.8
+        
+        # Informamos que comienza la grabación
+        print('Puedes comenzar a hablar')
+        
+        # Guardar audio
+        audio = r.listen(origin)
+        
+        try:
+            # Buscar en google lo que hemos escuchado
+            text = r.recognize_google(audio, language='es-es')
+            return text
+        except sr.UnknownValueError:
+            print('Ups, no te entendí')
+            return 'Esperando'
+        except sr.RequestError:
+            print('Ups, sin servicio')
+            return 'Esperando'
+        except:
+            print('Ups, algo ha salido mal')
+            return 'Esperando'
+        
+def talk(msg):
+    newVoiceRate = 180
+    
+    # Encender motor pyttsx3
+    engine = pyttsx3.init()
+    
+    engine.setProperty('voice', 'com.apple.eloquence.es-ES.Monica')
+    engine.setProperty('rate', newVoiceRate)
+    # Pronunciar mensjaje
+    engine.say(msg)
+    engine.runAndWait()
+    
+def get_voices():
+    engine = pyttsx3.init()
+    voices = engine.getProperty('voices')
+    for i, voice in enumerate(voices):
+        print(f"Voz {i}")
+        print(f"Id {voice.id}")
+        print(f"Nombre {voice.name}")
+        print(f"Idioma {voice.languages}")
+        print(f"Género {voice.gender}")
+        print(f"Género {voice.age}")
+        print(f"---------------------------")
+        
+def say_day():
+    day = datetime.date.today()
+    weekday = {
+        0: 'Lunes',
+        1: 'Martes',
+        2: 'Miércoles',
+        3: 'Jueves',
+        4: 'Viernes',
+        5: 'Sábado',
+        6: 'Domingo'
+    }
+    talk(f'Hoy es {weekday[day.weekday()]}')
+    
+def say_hour():
+    hour = datetime.datetime.now()
+    talk(f'En este momento son las {hour.hour} horas y {hour.minute} minutos')
+    
+def saludo():
+    hour = datetime.datetime.now()
+    if hour.hour < 6 or hour.hour > 20:
+        momento = 'Buenas noches.'
+    elif 6 <= hour.hour < 13:
+        momento = 'Buenos días.'
+    else:
+        momento = 'Buenas tardes.'
+    talk(f'{momento} Soy Lía, tu asistente personal. Por favor, dime en qué puedo ayudarte.')
+    
+def requests():
+    saludo()
+    stop = False
+    while not stop:
+        #Activar el micro y guardar la request en un string
+        request = audio_to_text().lower()
+        
+        if 'qué día es hoy' in request:
+            say_day()
+        elif 'qué hora es' in request:
+            say_hour()
